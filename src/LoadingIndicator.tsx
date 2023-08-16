ad266e89d6cd4cd0f16815fa9376853cbc36848f
hliu <hliu@proxya.com>
2023-08-16 18:42:28 +0200

    loading styles changed

--- src/LoadingIndicator.tsx
+++ src/LoadingIndicator.tsx
@@ -6,8 +6,8 @@ type Props = {};
 const LoadingIndicator: React.FC<Props> = () => {
   return (
     <View style={styles.activityOverlayStyle}>
-      <View style={styles.activityIndicatorContainer}>
-        <ActivityIndicator size="large" />
+      <View>
+        <ActivityIndicator size="large" color={'#00B0B9'} />
       </View>
     </View>
   );
@@ -16,24 +16,10 @@ const LoadingIndicator: React.FC<Props> = () => {
 const styles = StyleSheet.create({
   activityOverlayStyle: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'rgba(255, 255, 255, .5)',
     display: 'flex',
     justifyContent: 'center',
     alignContent: 'center',
     borderRadius: 5,
-  },
-  activityIndicatorContainer: {
-    backgroundColor: 'lightgray',
-    padding: 10,
-    borderRadius: 50,
-    alignSelf: 'center',
-    shadowColor: '#000000',
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowRadius: 5,
-    shadowOpacity: 1.0,
   },
 });
 
