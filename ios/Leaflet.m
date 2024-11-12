1b1b2701bc4ea6de3c6ca2b1e911aa6785290dd9
amine btc <amineoutmal1@gmail.com>
2024-11-12 16:37:23 +0100

    fix ios error

--- ios/Leaflet.m
+++ ios/Leaflet.m
@@ -2,6 +2,11 @@
 
 @interface RCT_EXTERN_MODULE(Leaflet, NSObject)
 
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
